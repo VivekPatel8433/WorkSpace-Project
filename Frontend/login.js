baaed1987cc0680c5c2bf5b375b82de22938ed68
Vivekbhai Patel <VivekPatel8433@gmail.com>
2025-08-06 15:59:03 -0600

    Backend

--- Frontend/login.js
+++ Frontend/login.js
@@ -14,6 +14,20 @@
 //     });
 // })
 
+// document.addEventListener("DOMContentLoaded" , () =>  {
+//     fetch('../file/headers.html')
+//     .then(res => res.text())
+//     .then(data => {
+//         document.getElementById("loginPageHeader").innerHTML = data;
+//     });
+
+//      fetch('../file/footers.html')
+//      .then(res => res.text())
+//      .then(data => {
+//         document.getElementById("loginPageFooter").innerHTML = data;
+//     });
+// })
+
 document.querySelector("form").addEventListener("submit", async function (e) {
   e.preventDefault();
 
@@ -25,7 +39,7 @@ document.querySelector("form").addEventListener("submit", async function (e) {
   let selectedRole = null;
   for (const radio of roleRadios) {
     if (radio.checked) {
-      selectedRole = radio.id;  
+      selectedRole = radio.id;  // Assuming id="owner" or "co-worker"
       break;
     }
   }
@@ -52,7 +66,7 @@ document.querySelector("form").addEventListener("submit", async function (e) {
         if (data.role === "owner") {
           window.location.href = "/owner-dashboard.html";
         } else if (data.role === "co-worker") {
-          window.location.href = "/coworker-dashboard.html";
+          window.location.href = "/coworker.html";
         }
       } else {
         alert("Role does not match the registered role for this user.");
