41a2465a0b9a7a852d3bd564b19eff28c6a286d1
Vivekbhai Patel <VivekPatel8433@gmail.com>
2025-08-14 11:00:15 -0600

    Backend

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3001;
 // Connect to DB
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://vivekpatel8433.github.io",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
