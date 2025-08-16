b0a258486b79a34e9ee32c90b8cc33f893cf4c1e
Vivekbhai Patel <VivekPatel8433@gmail.com>
2025-08-15 20:41:14 -0600

    Updated

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,13 +1,12 @@
-// middleware/auth.js
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // "Bearer <token>"
+  const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // must contain { id: userId } 
+    req.user = decoded; // decoded must include { id: <userId> }
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
